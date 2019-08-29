17a0d74ffea61d95d2947818be02282e37c69aee
EC2 Default User <ec2-user@ip-172-31-95-129.ec2.internal>
2019-08-29 02:54:58 +0000

    Added new lines to the paragraph

---
+++ ClassWork/lab1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Hello World</title>
+    </head>
+    <body>
+        <h1>Hello</h1>
+            <p>this is how to write a paragraph.  
+            Please just put the stuff in here
+            I can continue to write more stuff here
+            but i think i will stop while i am ahead.</p>
+
+<?php
+echo 'Hello World';
+?>
+    </body>
+</html>
