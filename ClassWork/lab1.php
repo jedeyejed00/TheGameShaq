e46741cd12ea8fbf9295272a96a08f9194ae6ef7
EC2 Default User <ec2-user@ip-172-31-95-129.ec2.internal>
2019-08-31 01:00:32 +0000

    Rene 8-30

--- ClassWork/lab1.php
+++ ClassWork/lab1.php
@@ -1,14 +1,36 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title> Hello World</title>
+        <title> Labwork 1: Hello World</title>
     </head>
     <body>
-        <h1>Hello</h1>
-            <p>this is how to write a paragraph.  
-            Please just put the stuff in here
-            I can continue to write more stuff here
-            but i think i will stop while i am ahead.</p>
+        <h1>Hello World I have arrived!</h1>
+            <p>Hello my name is Rene<br>
+            I am taking this class because i am really interested in creating dynamic website.  
+            I have created a few websites before but it has typically been with html.  
+            I would like to upgrade my game by incorporating php with some html.  
+            My educational goal has always been real simple learn as much as you can and as often as you can.  
+            I am a life time learner.  I think learning new things is one of the best things people can do.  
+            The only thing that trumps learning new things is executing and implementing the new things that i learn.  
+            As far as a career goal i am already in the IT field and i just want to continue to sharpen my IT saw 
+            since things in IT are always changing.  One of my worst fear in the career field is becoming obsolete 
+            with knowledge and technology in IT </p>
+            <br>
+            <hr><br>
+            Below is a list of my hobbies:
+            <ul> 
+                <li>Creating wooden signs</li> 
+                <li>Building old arcade games</li> 
+                <li>Surfing the internet</li> 
+                <li>Website creation</li> 
+            </ul>
+            <a href="https://github.com/jedeyejed00/TheGameShaq">Check out my profile GITHub</a>
+            <br>
+            <hr>
+            If you want to know more about me print "<p>My favorite website is <a href="http://www.facebook.com">Facebook</a>
+            My favorite website is <a href="http://www.offerup.com">OfferUp</a>
+            My favorite website is <a href="http://www.facebook.com/thegameshaq">TheGameShaq</a>
+            </p>";
 
 <?php
 echo 'Hello World';
