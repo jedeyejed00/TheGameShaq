aeb8c1c197e654b1607605ba820ff9d03497dac6
EC2 Default User <ec2-user@ip-172-31-95-129.ec2.internal>
2019-09-11 02:16:26 +0000

    My first commit

--- HomeWork2/process_contact.php
+++ HomeWork2/process_contact.php
@@ -1,5 +1,22 @@
 <?php 
-$FirstName = $_post['FirstName'];
+$FirstName = $_POST['FirstName'];
+$FirstName = $_POST['FirstName'];
+$LastName = $_POST['LastName'];
+$EmailAddress = $_POST['EmailAddress'];
+$gender = $_POST['gender'];
+$age = $_POST['age'];
+$letter = $_POST['letter'];
+$email = $_POST['email'];
+$info = $_POST['info'];
+$alone = $_POST['alone'];
+$call = $_POST['call'];
+$text = $_POST['text'];
+$ContributionAmount = $_POST['ContributionAmount'];
+$check = $_POST['check'];
+$comments = $_POST['comments']
 
-  echo '<h1> You logged in successfully! $FirstName</h1>';
+
+
+ echo "<p>email Address $age</p>";
+ 
 ?>
