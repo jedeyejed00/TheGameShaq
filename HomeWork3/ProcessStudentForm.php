bf88e308b4309bac14fdcda2204e03f4bba8e441
EC2 Default User <ec2-user@ip-172-31-95-129.ec2.internal>
2019-09-13 19:26:47 +0000

    My first commit

--- HomeWork3/ProcessStudentForm.php
+++ HomeWork3/ProcessStudentForm.php
@@ -5,23 +5,16 @@
         $units =$_POST['units']; 
         
         
-        $CostPerUnit = 241;
+        $costperunit = 241;
 
-        $ln = $_POST['ln']; 
 
-        $ea = $_POST['ea']; 
-
-        $g = $_POST['g']; 
-
-        $age = $_POST['age']; 
-
-        $ca = $_POST['ca']; 
 
       
 
 
-          echo "<p>Your total fees are :$;
-          echo $CostPerUnit * $units</P>"; 
+          echo "<p>Your total fees are :</p>";
+      
+          echo $units * $costperunit;
 
 
 
