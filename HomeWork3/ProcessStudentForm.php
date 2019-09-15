70b792e3924c2e43d1121d4a93ba2d1508f7de2a
EC2 Default User <ec2-user@ip-172-31-95-129.ec2.internal>
2019-09-15 23:09:22 +0000

    My first commit

--- HomeWork3/ProcessStudentForm.php
+++ HomeWork3/ProcessStudentForm.php
@@ -6,20 +6,18 @@
         $resident =$_POST['resident'];
         $costperunit = 46;
         $nonresident = 265;
+        $mandatoryfees = 20
 
 
 if ($resident < 1) {
-   echo $nonresident * $units;
+   echo $nonresident * $units + $mandatoryfees;
 }
 elseif ($resident = 1) {
-    echo $costperunit * $units;
+    echo $costperunit * $units + $mandatoryfees;
 }
 echo"<br>";
  echo "<h1> Thank  you for Registering!</h1>";
          
  echo"<br>";     
-
-
-
-          echo '<a href = "StudentForm.html">Back To Home Page</a>'; 
+echo '<a href = "StudentForm.html">Back To Home Page</a>'; 
 ?> 
