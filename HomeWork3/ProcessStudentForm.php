b22ef7fd3b55412ef05b9ce1f705d4bfac9bb0d0
EC2 Default User <ec2-user@ip-172-31-95-129.ec2.internal>
2019-09-17 02:26:29 +0000

    My first commit

--- HomeWork3/ProcessStudentForm.php
+++ HomeWork3/ProcessStudentForm.php
@@ -4,11 +4,11 @@
 
         $units =$_POST['units']; 
         $resident =$_POST['resident'];
+        $collegeservices= $_POST['collegeservice'];
+        $parking =$_POST['parking'];
         $costperunit = 46;
         $nonresident = 265;
         $mandatoryfees = 20;
-        $collegeservices = 20;
-        $parking = 30;
         $randomscholarship = (rand(0,500));
         $calcnonres = $nonresident * $units;
         $registrationcostnonres = $calcnonres + $mandatoryfees + $collegeservices + $parking;
@@ -37,8 +37,22 @@ echo "<center>";
 
 
 }
-elseif ($resident = 1) {
-    echo ($costperunit * $units + $mandatoryfees) - (rand(0,500));
+elseif ($resident = 1 and $collegeservices = 20 and $parking =30 );
+{
+    
+ echo"<br>";
+ echo "<h1> Thank  you for Registering!</h1>";
+ 
+echo "<h3>Cost of Tuition:$ $calcnonres </h3/>";
+echo "<h3>Student Health Fee:$ $mandatoryfees</h3/>";
+echo "<h3>College Services card:$ $collegeservices</h3/>";
+echo "<h3>Parking Permit:$ $parking</h3/>";
+echo "<h3>Total Registration Costs:$  $registrationcostnonres </h3/>";
+echo "<h3>Scholarship Award:$ $randomscholarship</h3/>";
+echo "<h1>Total College Balance Due:$ $totcostnonres  </h1/>";
+echo"<br>";     
+echo '<a href = "StudentForm.html">Back To Home Page</a>'; 
+echo "<center>";
 }
 
 ?> 
