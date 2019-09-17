b4c05f75d5f23ac3bb46aeb68127a4b1f0bea865
EC2 Default User <ec2-user@ip-172-31-95-129.ec2.internal>
2019-09-17 02:37:22 +0000

    My first commit

--- HomeWork3/ProcessStudentForm.php
+++ HomeWork3/ProcessStudentForm.php
@@ -18,9 +18,9 @@
        // $totcostres = $resident * $units + $collegeservices + $mandatoryfees -$randomscholarship;
         //$randomscholarship = (rand(0,500));
       
-
+//non Rez, No Parking, Yes College Service
 echo "<center>";
-if ($resident < 1)  {
+if ($resident = 1 and $collegeservices = 20 and $parking =0)  {
     echo"<br>";
  echo "<h1> Thank  you for Registering!</h1>";
  
@@ -34,13 +34,49 @@ echo "<h1>Total College Balance Due:$ $totcostnonres  </h1/>";
 echo"<br>";     
 echo '<a href = "StudentForm.html">Back To Home Page</a>'; 
 echo "<center>";
-
-
 }
+
+//non Rez, YES Parking, Yes College Service
 elseif ($resident = 1 and $collegeservices = 20 and $parking =30 );
 {
     
  echo"<br>";
+ echo "<h1> Thank  you for Registering!</h1>";
+ 
+echo "<h3>Cost of Tuition:$ $calcnonres </h3/>";
+echo "<h3>Student Health Fee:$ $mandatoryfees</h3/>";
+echo "<h3>College Services card:$ $collegeservices</h3/>";
+echo "<h3>Parking Permit:$ $parking</h3/>";
+echo "<h3>Total Registration Costs:$  $registrationcostnonres </h3/>";
+echo "<h3>Scholarship Award:$ $randomscholarship</h3/>";
+echo "<h1>Total College Balance Due:$ $totcostnonres  </h1/>";
+echo"<br>";     
+echo '<a href = "StudentForm.html">Back To Home Page</a>'; 
+echo "<center>";
+}
+
+//non Rez, YES Parking, NO College Service
+echo "<center>";
+if ($resident = 1 and $collegeservices = 0 and $parking =30)  {
+    echo"<br>";
+ echo "<h1> Thank  you for Registering!</h1>";
+ 
+echo "<h3>Cost of Tuition:$ $calcnonres </h3/>";
+echo "<h3>Student Health Fee:$ $mandatoryfees</h3/>";
+echo "<h3>College Services card:$ $collegeservices</h3/>";
+echo "<h3>Parking Permit:$ $parking</h3/>";
+echo "<h3>Total Registration Costs:$  $registrationcostnonres </h3/>";
+echo "<h3>Scholarship Award:$ $randomscholarship</h3/>";
+echo "<h1>Total College Balance Due:$ $totcostnonres  </h1/>";
+echo"<br>";     
+echo '<a href = "StudentForm.html">Back To Home Page</a>'; 
+echo "<center>";
+}
+
+//non Rez, No Parking, NO College Service
+echo "<center>";
+if ($resident = 1 and $collegeservices = 0 and $parking =0)  {
+    echo"<br>";
  echo "<h1> Thank  you for Registering!</h1>";
  
 echo "<h3>Cost of Tuition:$ $calcnonres </h3/>";
