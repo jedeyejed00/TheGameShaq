91c8eb04bb362e7061d1669f6018f5b948d031d3
EC2 Default User <ec2-user@ip-172-31-95-129.ec2.internal>
2019-09-19 01:57:05 +0000

    My first commit

--- ClassWork/Lab4/generator.php
+++ ClassWork/Lab4/generator.php
@@ -1,6 +1,12 @@
 <?php
 $numparagraphs = $_POST['paragraphs'];
-$text = "This is a test to see how thisgs works";
+$text =$POST['text'];
+
+//this function is looking for a space in the letters in the text variable
+$textexplode = explode(' ',$text);
+print_r($textexplode);
+
+//$text = "This is a test to see how thisgs works";
 
 for ($i =0; $i <$numparagraphs; $i++) {
    echo "<p>$text , </p>";
