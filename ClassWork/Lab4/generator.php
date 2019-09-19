2e2cf5beeb62910f22c0ee1acf22c0b07ea1c36d
EC2 Default User <ec2-user@ip-172-31-95-129.ec2.internal>
2019-09-19 02:03:13 +0000

    My first commit

--- ClassWork/Lab4/generator.php
+++ ClassWork/Lab4/generator.php
@@ -4,7 +4,11 @@ $text =$POST['text'];
 
 //this function is looking for a space in the letters in the text variable
 $textexplode = explode(' ',$text);
+$shuffledtext = shuffle($textexplode);
+
+
 print_r($textexplode);
+print_r($shuffledtext);
 
 //$text = "This is a test to see how thisgs works";
 
