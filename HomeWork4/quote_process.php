cc0649cbe75575b8d410743a19b89fe787db650f
EC2 Default User <ec2-user@ip-172-31-95-129.ec2.internal>
2019-09-23 02:33:29 +0000

    My first commit

--- HomeWork4/quote_process.php
+++ HomeWork4/quote_process.php
@@ -1,17 +1,22 @@
 <?php
+
+$text = $POST[tester];
+echo "Your original text is :";
+echo $tester;
+
+for ($i = 0; $i <= 10; $i++) {
+   echo "<p>$tester</p>";
+
+}
+
 //$numparagraphs = $_POST['paragraphs'];
 //$tester =$POST['tester'];
-$text = $POST[text];
 //this function is looking for a space in the letters in the text variable
 
 //$textexplode = explode(' ',$text);
 //$shuffledtext = shuffle($textexplode);
 
 //print_r($textexplode);
-
-echo "Your original text is :";
-echo $tester;
-
 //print_r($textexplode);
 
 //use implode to turn back into a string
@@ -23,13 +28,7 @@ echo $tester;
 //print_r($shuffledtext);
 
 //$text = "This is a test to see how thisgs works";
-
-for ($i = 0; $i <= 10; $i++) {
-   echo "<p>$tester,</p>";
   // echo '<p>' . $tester . '</p>';
     
-}
-
-
 
 ?>
