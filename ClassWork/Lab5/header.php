5b926a9bea06d18ca4b4c10cb36d2939b8603222
EC2 Default User <ec2-user@ip-172-31-95-129.ec2.internal>
2019-09-26 01:56:51 +0000

    My first commit

---
+++ ClassWork/Lab5/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html> 
+
+<html> 
+
+<head><meta charset = "utf-8" </head> 
+<title>Homepage</title>
+<style> 
+
+body {background-color: coral;} 
+</style> 
+ 
+<header><p>This is the header</p></header>
---
+++ ClassWork/Lab5/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html> 
+
+<html> 
+
+<head><meta charset = "utf-8" </head> 
+<title>Homepage</title>
+<style> 
+
+body {background-color: coral;} 
+</style> 
+ 
+<header><p>This is the header</p></header>
+ 
+
+<main><h1></h1>This is the home page</h1></main>
+
+<footer><p>This is the footer</p></footer>
+
+<body> 
+
+</body>
+</html>
