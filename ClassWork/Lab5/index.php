c481adad319f1e15ed0928600d111d715b45b2fb
EC2 Default User <ec2-user@ip-172-31-95-129.ec2.internal>
2019-09-26 02:17:00 +0000

    My first commit

--- ClassWork/Lab5/index.php
+++ ClassWork/Lab5/index.php
@@ -9,12 +9,13 @@
 body {background-color: coral;} 
 </style> 
  
+ <?php include('includes/header.php');?>
 <header><p>This is the header</p></header>
  
 
-<main><h1></h1>This is the home page</h1></main>
+<main><h1></h1>This is the home page33</h1></main>
 
-<footer><p>This is the footer</p></footer>
+ <?php include('includes/footer.php');?>
 
 <body> 
 
