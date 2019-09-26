6cea1dec1ab4823ace90c4004c0fb8182f05e96f
EC2 Default User <ec2-user@ip-172-31-95-129.ec2.internal>
2019-09-26 02:25:27 +0000

    My first commit

--- ClassWork/Lab5/footer.php
+++ ClassWork/Lab5/footer.php
@@ -1,5 +1,6 @@
-echo "<footer>
-  <p>Posted by: Hege Refsnes</p>
-  <p>Contact information: <a href="mailto:someone@example.com">
-  someone@example.com</a>.</p>
-</footer>"
+
+
+
+
+
+ <?php include('includes/footer.php');?>
--- ClassWork/Lab5/index.php
+++ ClassWork/Lab5/index.php
@@ -10,10 +10,14 @@ body {background-color: coral;}
 </style> 
  
  <?php include('includes/header.php');?>
+ 
+ 
 <header><p>This is the header</p></header>
  
 
-<main><h1></h1>This is the home page33</h1></main>
+<main><h1>This is the home page</h1></main>
+
+<footer><p>This is the header</p></footer>
 
  <?php include('includes/footer.php');?>
 
