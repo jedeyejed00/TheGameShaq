65b62a5e1b5109977ba661094fd8ea08daf49413
EC2 Default User <ec2-user@ip-172-31-95-129.ec2.internal>
2019-10-01 03:29:19 +0000

    My first commit

--- ClassWork/Lab5/includes/header.php
+++ ClassWork/Lab5/includes/header.php
@@ -3,6 +3,19 @@
     <head>
          <meta charset="utf-8">
          <title>Homepage</title>
+        
+<style>
+.header {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: red;
+   color: white;
+   text-align: center;
+}
+</style>
+
 
 
     </head>
