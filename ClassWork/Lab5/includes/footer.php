a4ad24cef25254d2cc9e2b2b85e151437cb8669d
EC2 Default User <ec2-user@ip-172-31-95-129.ec2.internal>
2019-10-01 03:24:21 +0000

    My first commit

--- ClassWork/Lab5/includes/footer.php
+++ ClassWork/Lab5/includes/footer.php
@@ -1,9 +1,18 @@
 <html>
-    #site-footer {
-height: 50px;
-background: red;
+<head>
+<style>
+.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: red;
+   color: white;
+   text-align: center;
 }
+</style>
 
+</head>
 <body>
 <h1>FOOTER</h1>
 </body>
