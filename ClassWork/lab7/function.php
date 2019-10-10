b81cb036772b91c931a94b1cdbb3f34b7770b140
EC2 Default User <ec2-user@ip-172-31-95-129.ec2.internal>
2019-10-10 01:22:11 +0000

    My first commit

---
+++ ClassWork/lab7/function.php
@@ -0,0 +1,20 @@
+<?php
+
+function greeting(){
+    echo '<p> hello my name is rene L</p>';
+}
+
+greeting();
+
+//<p>hello my name is xxx </p>
+
+//<p>hello my name is aaa</p/>
+
+//<p>hello my name is bbb</p>
+
+//<p>hello my name is ccc</p>
+
+//<p>hello my name is ddd</p>
+
+
+?>
---
+++ ClassWork/lab7/function2.php
@@ -0,0 +1,22 @@
+<?php
+
+$nametest = "Mario"
+
+function greeting($nametest){
+    echo '<p> hello my name is '.$nametest.'</p>';
+}
+
+greeting();
+
+//<p>hello my name is xxx </p>
+
+//<p>hello my name is aaa</p/>
+
+//<p>hello my name is bbb</p>
+
+//<p>hello my name is ccc</p>
+
+//<p>hello my name is ddd</p>
+
+
+?>
