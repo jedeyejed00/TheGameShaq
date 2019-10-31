95f898b0e355791a1393ff8befc92972a386daf4
EC2 Default User <ec2-user@ip-172-31-95-129.ec2.internal>
2019-10-31 03:34:15 +0000

    My first commit

--- ClassWork/lab10/crud.php
+++ ClassWork/lab10/crud.php
@@ -4,8 +4,18 @@
 *   CHECK IF THE FORM HAS BEEN SUBMITTED AND INSERT
 *   NEW USER INTO THE DATABASE
 */
+
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
 }
+
+$first_name = $POST['first_name'];
+$first_name = $POST['last_name'];
+$first_name = $POST['email'];
+$first_name = $POST['password'];
+
+$insert_query = "INSERT INTO user_lopez(first_name, last_name, email, password)
+		VALUES ($first_name, $last_name, $email, $password)";
+		$result = mysqli_query($connection, $insert_query);
 /*
 *   QUERY THE DATABASE AND STORE ALL USERS INTO A VARIABLE
 */
