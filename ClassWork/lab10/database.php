071f4987ce2cb8734310db4c8eb17f2a7e0c2b57
EC2 Default User <ec2-user@ip-172-31-95-129.ec2.internal>
2019-10-31 03:47:42 +0000

    My first commit

--- ClassWork/lab10/database.php
+++ ClassWork/lab10/database.php
@@ -2,8 +2,8 @@
 // Create environment variables
 $hostname   = getenv('DB_HOSTNAME');
 $username   = getenv('DB_USERNAME_PUBLIC');
-$password   = getenv('DB_PW_PUBLIC');
-$dbname     = getenv('DB_NAME');
+$password   = getenv('DB_PW_CLASS');
+$dbname     = getenv('DB_NAME_DB');
 // Establish the connection to the database
 $connection = mysqli_connect($hostname, $username, $password, $dbname);
 ?>
